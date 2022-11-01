548ed9363283656636c95e69636306e280a1694b
jora <orantes.o.j@outlook.com>
2022-11-01 16:36:43 -0600

    infinite scroll added

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -4,39 +4,41 @@ import styles from "./MoviesGrid.module.css";
 import { get } from "../utils/httpClient";
 import { Spinner } from "./Spinner";
 import {useQuery} from "../hooks/useQuery";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-
-export function MoviesGrid() {
-  //let movies = [];
-  //const moviesState = useState([]);
-  //const movies = moviesState[0];
-  //const setMovies = moviesState[1];
-  //const [movies, setMovies] = moviesState;
+export function MoviesGrid({ search }) {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-  const query = useQuery();
-  const search = query.get("search");
+  const [page, setPage] = useState(1);
+  const [hasMore, sethasMore] = useState(true);
 
   useEffect(() => {
     setIsLoading(true);
-    const searchUrl = search ? "/search/movie?query=" + search : "/discover/movie";
+    const searchUrl = search 
+    ? "/search/movie?query=" + search + "&page=" + page
+    : "/discover/movie?page=" + page;
     get(searchUrl)
       .then((data) => {
-        setMovies(data.results);
+        setMovies((prevMovies) => prevMovies.concat(data.results));
+        sethasMore(data.page < data.total_pages);
         setIsLoading(false);
       });
       // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [search]);
+  }, [search, page]);
 
-  if(isLoading){
-    return <Spinner/>;
-  }
+
   return (
-    <ul className={styles.moviesGrid}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
+    <InfiniteScroll 
+    dataLength={movies.length}
+    hasMore={hasMore}
+    next={() => setPage((prevPage) => prevPage + 1)}
+    loader={<Spinner/>}
+    >
+      <ul className={styles.moviesGrid}>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </ul>
+    </InfiniteScroll>
   );
 }
